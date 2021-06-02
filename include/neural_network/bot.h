00951a77d3df742f707c05ddfc96f7e00a42f53e
alan gueret <alan.gueret@epita.fr>
2021-06-02 21:56:45 +0200

    start creating files

---
+++ include/neural_network/bot.h
@@ -0,0 +1,25 @@
+#include "neural_struct.h"
+
+#pragma once
+
+void print_bot(bot *b);
+
+void build_bot(bot *b);
+
+void free_bot(bot *b);
+
+void copy_bot(bot *b, bot *dest, char mutated);
+
+void crossover(bot *b, bot *partner, bot *dest);
+
+void mutate_bot(bot *b);
+
+void mix_bot(bot *b, bot *partner);
+
+size_t scoring(float *resultat, float *expected, size_t size);
+
+void play_bot(bot *b);
+
+void save_bot(bot *b, char *path);
+
+bot *load_bot(char *path);
